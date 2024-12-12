195f51a9a646255046243f4223188b1e33fd9875
Augusto Portillo <porti.sk8@gmail.com>
2024-12-12 14:22:06 -0300

    WIP - SK.Agentes

---
+++ src/SK.Agentes/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.SemanticKernel;
+
+Console.WriteLine("Let's talk!");
+
+var builder = Kernel.CreateBuilder();
+
+
+builder.AddOpenAIChatCompletion(/*<...service configuration>*/, serviceId: "service-1");
+builder.AddAzureOpenAIChatCompletion(/*<...service configuration>*/, serviceId: "service-2");
+
+Kernel kernel = builder.Build();
+
+ChatCompletionAgent agent =
+	new()
+	{
+		Name = "<agent name>",
+		Instructions = "<agent instructions>",
+		Kernel = kernel,
+		Arguments = // Specify the service-identifier via the KernelArguments
+		  new KernelArguments(
+			new OpenAIPromptExecutionSettings()
+			{
+				ServiceId = "service-2" // The target service-identifier.
+			});
+	};
